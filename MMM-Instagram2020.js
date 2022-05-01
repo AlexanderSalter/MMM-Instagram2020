6f8254beaa2c5f259abec9b82c49e15b308449db
AlexanderSalter <bigal@alswebpage.net>
2022-05-01 14:53:23 +0100

    Updated Instagram Authorisation Process to be interactive. Usage of this module now requires MagicMirror to use https/ssl as this is required by the Instagram Graph API

--- MMM-Instagram2020.js
+++ MMM-Instagram2020.js
@@ -5,21 +5,18 @@
  * MIT Licensed.
  */
 
-
 Module.register("MMM-Instagram2020",{
 	// Default module config.
 	defaults: {
 		format: 'json',
-        lang: 'en-us',
+        	lang: 'en-us',
 		client_id: '',			// Facebook Instagram App ID
 		client_secret: '',		// Facebook Instagram App Secret 
 		redirect_uri: '',		// Facebook Instagram App redirect_uri
-		auth_code: '',
-		access_token: '',	//
+		access_token: '',		// Facebook Instagram Access Token
 		animationSpeed: 1000,	//1 second
-        updateInterval: 10000,	// 10 seconds
+	        updateInterval: 10000,	// 10 seconds
 	},
-	
 	// Start Module
 	start: function() {
 		var self = this;
@@ -32,17 +29,15 @@ Module.register("MMM-Instagram2020",{
 		setTimeout(function (){
 			self.doAuthentication();
 		},10);
-	},	
-
+	},
 	// Override dom generator.
 	getDom: function() {
 		var self = this;
 		var wrapper = document.createElement("div");
 		wrapper.classList.add("small");
 		var imageDisplay = document.createElement('div');
-		
 		if(self.stage === "load" || self.stage === "load_images"){
-			Log.log("Loading Placeholder");
+			Log.log(self.name + ': Loading Placeholder');
 			var imageLink = document.createElement('div');
 			imageLink.id = "MMM-Instagram-image";
 			imageLink.innerHTML = "<img src='" + self.data.path + "ig_placeholder.png' width='100%'>";
@@ -51,13 +46,13 @@ Module.register("MMM-Instagram2020",{
 			imageLink.innerHTML += "<p class='light xsmall' style='text-align: center;'>"+now.format('MMMM Do YYYY @ HH:mm')+"</p>";
 		}
 		else if(self.stage === "auth_link"){
-			Log.log("Loading Pre Auth Page");
+			Log.log(self.name + ': Loading Pre Auth Page');
 			var imageLink = document.createElement('div');
 			imageLink.id = "MMM-Instagram-image";
 			imageLink.innerHTML += "<p class='light' style='text-align: center;'>Click <A HREF='"+self.authorise_url+"' target='_new'>HERE</A> to authorise access to your instagram account.</p>";
 		}
 		else if(self.stage === "show_images"){
-			Log.log("Loading Images");
+			Log.log(self.name + ': Loading Images');
 			if (self.activeItem >= self.images.photo.length) {
 				self.activeItem = 0;
 			}
@@ -69,66 +64,54 @@ Module.register("MMM-Instagram2020",{
 			imageLink.innerHTML += "<p class='light xsmall' style='text-align: center;'>"+tempimage.timestamp+"</p>";
 		}
 		imageDisplay.appendChild(imageLink);
-        wrapper.appendChild(imageDisplay);
-        return wrapper;
+        	wrapper.appendChild(imageDisplay);
+	        return wrapper;
 	},
-	
 	getStyles: function() {
-        return ['instagram.css', 'font-awesome.css'];
-    },
-	
+        	return ['instagram.css', 'font-awesome.css'];
+	},
 	doAuthentication: function() {
 		var self = this;
-		Log.log(self.name + ' sending INSTAGRAM_AUTH notification');
+		Log.log(self.name + ': sending INSTAGRAM_AUTH notification');
 		self.sendSocketNotification("INSTAGRAM_AUTH", null);
 	},
-	 
 	scheduleUpdateInterval: function() {
-        var self = this;
-        Log.info("Scheduled update interval set up...");
-        self.updateDom(self.config.animationSpeed);
-        setInterval(function() {
-            Log.info("incrementing the activeItem and refreshing");
-            self.activeItem++;
-            self.updateDom(self.config.animationSpeed);
-        }, this.config.updateInterval);
-    },
-
+	        var self = this;
+        	Log.info(self.name + ': Scheduled update interval set up to' + self.config.animationSpeed);
+	        self.updateDom(self.config.animationSpeed);
+		setInterval(function() {
+			Log.info(self.name + ': incrementing the activeItem and refreshing');
+			self.activeItem++;
+			self.updateDom(self.config.animationSpeed);
+	        }, this.config.updateInterval);
+	},
 	// override socketNotificationReceived
-    socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function(notification, payload) {
 		var self = this;
-        Log.log('socketNotificationReceived: ' + notification);
-        if (notification === 'INSTAGRAM_ACCESS_TOKEN')
-        {
-            Log.log('received INSTAGRAM_ACCESS_TOKEN: ' + payload);
+	        Log.log(self.name + ': socketNotificationReceived: ' + notification);
+        	if (notification === 'INSTAGRAM_ACCESS_TOKEN') {
+		        Log.log(self.name + ': received access_token: ' + payload);
 			self.config.access_token = payload;
 			if(payload != ""){
 				//Token Exisits
-				Log.log('Token Exists in accesstoken.cfg');
+				Log.log(self.name + ': Token Exists in accesstoken.cfg');
 				self.stage = "load_images";
-				self.config.access_token = payload;
+				Log.log(self.name + ': Loading Images');
+                                setTimeout(function (){
+                                        self.sendSocketNotification("INSTAGRAM_MEDIA_REFRESH", null);
+                                },10);
 				Log.log(self.name + ' sending INSTAGRAM_AUTH_REFRESH notification');
 				self.sendSocketNotification("INSTAGRAM_AUTH_REFRESH", null);
 			}
 			else{
 				//Token Does Not Exisit
-				Log.log('Token Does Not Exist in accesstoken.cfg');
-				if(self.config.auth_code != ""){
-					//Auth Code Exisits
-					Log.log('Auth Code Exists in config.js');
-					self.stage = "auth_exchange";
-					self.sendSocketNotification('INSTAGRAM_AUTH_EXCHANGE', null);
-				}
-				else{
-					//Auth Code Does Not Exisit
-					Log.log('Auth Code Does Not Exist in config.js');
-					self.stage = "auth_link";
-					self.sendSocketNotification('INSTAGRAM_AUTH_NEW', null);
-					self.updateDom(self.config.animationSpeed);
-				}
+				Log.log(self.name + ': Token Does Not Exist in accesstoken.cfg');
+				self.stage = "auth_link";
+				self.sendSocketNotification('INSTAGRAM_AUTH_NEW', null);
+				self.updateDom(self.config.animationSpeed);
 				self.sendSocketNotification('MMM-Instagram2020-CONFIG', this.config);
 			}
-        }
+		}
 		else if (notification === 'INSTAGRAM_ACCESS_TOKEN_NEW'){
 			Log.log('received INSTAGRAM_ACCESS_TOKEN_NEW: ' + payload);
 			if(payload != ""){
@@ -142,33 +125,26 @@ Module.register("MMM-Instagram2020",{
 			}
 			else{
 				//Token Does Not Exisit
-				Log.log('Token Does Not Exist in accesstoken.cfg');
-				if(self.config.auth_code != ""){
-					//Auth Code Exisits
-					Log.log('Auth Code Exists in config.js');
-					self.stage = "auth_exchange";
-					self.sendSocketNotification('INSTAGRAM_AUTH_EXCHANGE', null);
-				}
-				else{
-					//Auth Code Does Not Exisit
-					Log.log('Auth Code Does Not Exist in config.js');
-					self.stage = "auth_link";
-					self.sendSocketNotification('INSTAGRAM_AUTH_NEW', null);
-					self.updateDom(self.config.animationSpeed);
-				}
+				Log.log(self.name + ': Token Does Not Exist in accesstoken.cfg');
+				//Auth Code Does Not Exisit
+				Log.log(self.name + ': Auth Code Does Not Exist in config.js');
+				self.stage = "auth_link";
+				self.sendSocketNotification('INSTAGRAM_AUTH_NEW', null);
+				self.updateDom(self.config.animationSpeed);
 				self.sendSocketNotification('MMM-Instagram2020-CONFIG', this.config);
 			}
 		}
 		else if(notification === 'INSTAGRAM_AUTH_URL'){
-			Log.log('received INSTAGRAM_AUTH_URL: ' + payload);
+			self.stage = "auth_link";
+			Log.log(self.name + ': received INSTAGRAM_AUTH_URL: ' + payload);
 			self.authorise_url = payload;
 			self.updateDom(self.config.animationSpeed);
 		}
 		else if (notification === 'INSTAGRAM_IMAGE_ARRAY'){
-			Log.log('received INSTAGRAM_IMAGE_ARRAY: ' + payload);
+			Log.log(self.name + ': received INSTAGRAM_IMAGE_ARRAY: ' + payload);
 			self.images = payload;
 			self.stage = "show_images";
-            self.scheduleUpdateInterval();
+            		self.scheduleUpdateInterval();
 		}
-    }
+	}
 });
--- node_helper.js
+++ node_helper.js
@@ -9,60 +9,49 @@ var NodeHelper = require('node_helper');
 var fs = require('fs');
 var request = require('request');
 var moment = require('moment');
+var url = require("url");
 
 module.exports = NodeHelper.create({
-    // subclass start method.
-    start: function() {
+	// subclass start method.
+	start: function() {
 		var self = this;
-        console.log("Starting node_helper for module [" + self.name + "]");
-		self.access_token = 'not set';
+	        console.log("Starting node_helper for module [" + self.name + "]");
 		self.config = new Array();
 		self.AccessTokenFile = self.path + "/accesstoken.cfg";
 		self.readAccessToken();
-    },
-    
-    // subclass socketNotificationReceived
-    socketNotificationReceived: function(notification, payload){
+		this.createRoutes();
+	},
+	// subclass socketNotificationReceived
+	socketNotificationReceived: function(notification, payload){
 		var self = this;
-        console.log("=========== notification received: " + notification);
-        if (notification === 'MMM-Instagram2020-CONFIG') {
-			//Get the config.
+	        console.log("=========== notification received: " + notification);
+	        if (notification === 'MMM-Instagram2020-CONFIG') {
+			console.log("MMM-Instagram2020 : Reading Config");
 			 self.config = payload;
 		}
-        else if (notification === 'INSTAGRAM_AUTH') {
-			//Get the auth code.
-			self.sendSocketNotification("INSTAGRAM_ACCESS_TOKEN", self.access_token);            
-        }
+	        else if (notification === 'INSTAGRAM_AUTH') {
+			self.readAccessToken();
+	        }
 		else if (notification === 'INSTAGRAM_AUTH_NEW') {
-			//No Temp Auth Code In Config so Show new auth link.
 			self.doNewAuthentication();           
-        }
-		else if (notification === 'INSTAGRAM_AUTH_EXCHANGE') {
-			self.doAuthExchange();
-		}
+	        }
 		else if (notification === 'INSTAGRAM_AUTH_REFRESH') {
 			self.doRefreshAuthentication();          
-        }
+	        }
 		else if (notification === 'INSTAGRAM_MEDIA_REFRESH') {
 			self.getImages();
 		}
-    },
-	
+	},
 	getImages() {
 		var self = this;
-		var api_url = 'https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp&access_token=' +  self.access_token;
-		request({url: api_url, method: 'GET'}, function(error, response, body) 
-        {
-            if (!error && response.statusCode == 200) 
-            {
-                // get our authentication token from the response
+		var api_url = 'https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp&access_token=' +  self.config.access_token;
+		request({url: api_url, method: 'GET'}, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+	        	        // get our authentication token from the response
 				var items = JSON.parse(body).data;
-				
 				var images = {};
-                images.photo = new Array();
-				
-				for (var i in items)
-                {
+		                images.photo = new Array();
+				for (var i in items) {
 					var id = items[i].id;
 					var media_type = items[i].media_type;
 					if (media_type == "VIDEO"){
@@ -73,52 +62,46 @@ module.exports = NodeHelper.create({
 					}
 					var caption = items[i].caption;
 					var timestamp = moment(items[i].timestamp);
-					
 					images.photo.push( {
-                        "type" : media_type,
-                        "photolink" : media_url,
+	        		                "type" : media_type,
+	                        		"photolink" : media_url,
 						"caption" : caption,
 						"timestamp" : timestamp.format('MMMM Do YYYY @ HH:mm'),
-                    });
+			                });
 				}
-				console.log(images);
+				//console.log(images);
 				self.sendSocketNotification('INSTAGRAM_IMAGE_ARRAY', images);
-            }
-            else
-            {
-                console.log("MMM-Instagram2020 Helper Image Get Error: " + response.statusCode);
-            }
-        });
+			}
+		        else {
+		                console.log("MMM-Instagram2020 : Image Get Error: " + response.statusCode);
+	        	}
+	        });
 	},
-	
-	
 	doRefreshAuthentication: function() {
         var self = this;
-		var api_url = 'https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=' + self.access_token;
-		request({url: api_url, method: 'GET'}, function(error, response, body) 
-        {
-            if (!error && response.statusCode == 200) 
-            {
-                // get our authentication token from the response
-                var response_values = JSON.parse(body);
-				console.log(response_values['access_token']); 
-				self.access_token = response_values['access_token'];
-				self.writeAccessToken(self.access_token);
-				self.sendSocketNotification("INSTAGRAM_ACCESS_TOKEN_NEW", self.access_token);  				
-            }
-			else if (!error && response.statusCode == 400) 
-			{
-				console.log("MMM-Instagram2020 Helper Refresh Auth Token Error: " + response.statusCode);
-				console.log(body);
-				
+		var api_url = 'https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=' + self.config.access_token;
+		request({url: api_url, method: 'GET'}, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+		                // get our authentication token from the response
+                		var response_values = JSON.parse(body);
+				console.log("MMM-Instagram2020 : Refreshed access_token: " + response_values['access_token']);
+				self.config.access_token = response_values['access_token'];
+				self.writeAccessToken(self.config.access_token);
+				self.sendSocketNotification("INSTAGRAM_ACCESS_TOKEN_NEW", self.config.access_token);
 			}
-            else
-            {
-                console.log("MMM-Instagram2020 Helper Refresh Auth Token Error: " + response.statusCode);
-            }
-        });
-    },
-	
+			else if (!error && response.statusCode == 400) {
+				console.log("MMM-Instagram2020 : Refresh access_token Error: " + response.statusCode);
+				console.log(body);
+				console.log("MMM-Instagram2020 : Resetting authentication");
+				self.doNewAuthentication();
+			}
+			else {
+		                console.log("MMM-Instagram2020 : Refresh access_token Error: " + response.statusCode);
+				console.log("MMM-Instagram2020 : Resetting authentication");
+                                self.doNewAuthentication();
+		        }
+	        });
+	},
 	doAuthExchange: function() {
 		var self = this;
 		var api_url = 'https://api.instagram.com/oauth/access_token';
@@ -128,78 +111,93 @@ module.exports = NodeHelper.create({
 		form_array['grant_type']='authorization_code';
 		form_array['redirect_uri']=self.config.redirect_uri;
 		form_array['code']=self.config.auth_code;
-		console.log(api_url);
-		console.log(form_array);
-		request({url: api_url, method: 'POST', form: form_array}, function(error, response, body) 
-        {
-            if (!error && response.statusCode == 200) 
-            {
-                // get our authentication token from the response
-                var response_values = JSON.parse(body);
-                console.log(response_values['access_token']);
+		console.log("MMM-Instagram2020 : Access Token Request Url" + api_url);
+		console.log("MMM-Instagram2020 : Access Token details" + form_array);
+		request({url: api_url, method: 'POST', form: form_array}, function(error, response, body) {
+		        if (!error && response.statusCode == 200) {
+		                // get our authentication token from the response
+		                var response_values = JSON.parse(body);
+		                console.log("MMM-Instagram2020 : Recieved new  access_token:" + response_values['access_token']);
 				var api_url = 'https://graph.instagram.com/access_token';
 				api_url += '?grant_type=ig_exchange_token';
 				api_url += '&client_secret='+self.config.client_secret;
 				api_url += '&access_token='+response_values['access_token'];
 				request({url: api_url, method: 'GET'}, function(error, response, body) {
-					if (!error && response.statusCode == 200) 
-					{
+					if (!error && response.statusCode == 200) {
 						// get our authentication token from the response
 						var response_values = JSON.parse(body);
-						console.log(body);
-						self.access_token = response_values['access_token'];
-						console.log(self.access_token);  
-						self.writeAccessToken(self.access_token);
-						self.sendSocketNotification("INSTAGRAM_ACCESS_TOKEN_NEW", self.access_token);						
+						console.log("MMM-Instagram2020 : Error getting new access_token:" + body);
+						self.config.access_token = response_values['access_token'];
+						console.log(self.config.access_token);
+						self.writeAccessToken(self.config.access_token);
+						self.sendSocketNotification("INSTAGRAM_ACCESS_TOKEN_NEW", self.config.access_token);
 					}
-					else if (!error && response.statusCode == 400) 
-					{
-						console.log("MMM-Instagram2020 Helper Auth Extend Error: " + response.statusCode);
+					else if (!error && response.statusCode == 400) {
+						console.log("MMM-Instagram2020 : Error getting new access_token: " + response.statusCode);
 						console.log(body);
-						
+						console.log("MMM-Instagram2020 : Resetting authentication");
+		                                self.doNewAuthentication();
 					}
-					else
-					{
-						console.log("MMM-Instagram2020 Helper Auth Extend Error: " + response.statusCode);
+					else {
+						console.log("MMM-Instagram2020 : Error getting new access_token: " + response.statusCode);
+						console.log("MMM-Instagram2020 : Resetting authentication");
+		                                self.doNewAuthentication();
 					}
 				});
-            }
-			else if (!error && response.statusCode == 400) 
-			{
-				console.log("MMM-Instagram2020 Helper Auth Exchange Error: " + response.statusCode);
-				console.log(body);				
 			}
-            else
-            {
-                console.log("MMM-Instagram2020 Helper Auth Exchange Error: " + response.statusCode);
-            }
-        });
+			else if (!error && response.statusCode == 400) {
+				console.log("MMM-Instagram2020 : Auth Exchange Error: " + response.statusCode);
+				console.log(body);
+			}
+			else {
+	                	console.log("MMM-Instagram2020 : Auth Exchange Error: " + response.statusCode);
+	        	}
+	        });
 	},
-	
 	doNewAuthentication: function() {
 		var self = this;
 		var api_url = 'https://api.instagram.com/oauth/authorize';
+		console.log("MMM-Instagram2020 : Creating OAuth URL for Instagram");
 		api_url += '?client_id='+self.config.client_id;
 		api_url += '&redirect_uri='+self.config.redirect_uri;
 		api_url += '&scope=user_profile,user_media';
 		api_url += '&response_type=code';
 		self.sendSocketNotification('INSTAGRAM_AUTH_URL', api_url);
 	},
-	
 	readAccessToken: function(){
 		var self = this;
+		console.log("MMM-Instagram2020 : Reading accesstoken.cfg");
 		fs.readFile(this.AccessTokenFile, "UTF8", function (err, data) {
 			if (err) throw err;
-			self.access_token = data;
-			//console.log(self.access_token);
+			self.config.access_token = data;
+			console.log("MMM-Instagram2020 : Read accesstoken.cfg: "+self.config.access_token);
+			self.sendSocketNotification("INSTAGRAM_ACCESS_TOKEN", self.config.access_token);
 		});
 	},
-	
 	writeAccessToken: function(data){
 		var self = this;
+		console.log("MMM-Instagram2020 : Writing accesstoken.cfg");
 		fs.writeFile(this.AccessTokenFile, data, function (err) {
 			if (err) throw err;
+			console.log("MMM-Instagram2020 : Wrote accesstoken.cfg: "+data);
 		});
 	},
-	
+	createRoutes: function() {
+		var self = this;
+		this.expressApp.get("/MMM-Instagram2020/auth", function(req, res) {
+			var query = url.parse(req.url, true).query;
+			if(query.code != ""){
+				//Recieved Auth Code
+				console.log("MMM-Instagram2020 : Auth Code Recieved:" + query.code);
+				self.config.auth_code = query.code;
+				console.log("MMM-Instagram2020 : Exchange Auth Code for Short Lived Tken:");
+				self.doAuthExchange();
+				res.send("MMM-Instagram2020 : Auth Code Received: " + query.code + "<BR><BR>You can Now close this window.");
+			}
+			else{
+				console.log("No Auth Code Returned");
+				res.send("MMM-Instagram2020 : Auth Code Not Received!");
+			}
+		});
+	},
  });
